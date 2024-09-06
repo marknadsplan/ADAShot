ba44e4c6df04c4453bf1714e05369c6b6cbd6700
Adrian Rosca <adrian@rosca.se>
2024-09-06 22:04:37 +0200

    Project name

--- compile.bat
+++ compile.bat
@@ -3,4 +3,4 @@ if not exist obj mkdir obj
 if not exist bin mkdir bin
 g++ -c -DBUILDING_DLL src/cpp/mylib.cpp -o obj/mylib.o
 g++ -shared -o bin/mylib.dll obj/mylib.o -Wl,--out-implib,bin/libmylib.a
-gprbuild -P project.gpr
+gprbuild -P ADAShot.gpr
